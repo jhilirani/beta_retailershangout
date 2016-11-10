ad52c1331c76fc57a59090d4df6d1d0ed8f140fb
jhilirani <jhilirani@gmail.com>
2016-11-11 01:43:48 +0530

    checking

--- beta/application/controllers/Index.php
+++ beta/application/controllers/Index.php
@@ -38,7 +38,7 @@ class Index extends MY_Controller{
         $data['slider1']=$slider1;
         
         $slider2=$this->Banner_model->get_home_slider(2);
-        //pre($slider2);die;
+        pre($slider2);die;
         $data['slider2']=$slider2;
         
         $noOfItem=  $this->Siteconfig_model->get_value_by_name('HOME_PAGE_NEW_ARRIVAL_ITEM_NO');
