ee5bce431661d96a2407d77ea5f2850653b9300e
jhilirani <jhilirani@gmail.com>
2016-11-26 21:58:06 +0530

    fixing iss

--- beta/application/views/home.php
+++ beta/application/views/home.php
@@ -57,7 +57,7 @@
             <div class="logo-slider-inner">
                 <div id="brand-slider" class="owl-carousel brand-slider custom-carousel owl-theme">
                     <?php foreach($brandZoneArr AS $k){
-                            if($k->brandImage==""){$src=SiteImagesURL.'no-image.png';}else{$src=SiteResourcesURL.'brand/original/'.$k->brandImage;}?>
+                            if($k->brandImage==""){$src=SiteImagesURL.'no-image.png';}else{$src=SiteResourcesURL.'brand/categroy/'.$k->brandImage;}?>
                     <div class="item">
                         <a href="<?php echo BASE_URL.'brand/'.base64_encode($k->brandId*226201).'/?';?>&brand=<?php echo $k->title;?>" class="image">
                         <img src="<?php echo $src;?>" alt="<?php echo $k->title;?>" title="<?php echo $k->title;?>"/>
